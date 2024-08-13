a9ab799e831955bb9b5435c914664b484fd26fa4
Luckydad60 <Luckydad60@gmail.com>
2024-08-12 20:50:47 -0700

    Few lines of codes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001
 
 const app = express();
-// app.use(express.static(path.resolve(__dirname, "../build")));
+app.use(express.static(path.resolve(__dirname, "../build")));
 app.use(cors())
 app.use(bodyParser.json());
 
@@ -58,9 +58,9 @@ app.post("/api/contact", bodyParser.urlencoded({ extended:false}), (req, res) =>
         }
     });
 })
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+})
 app.listen(PORT, () => {
     console.log(`server is live on PORT:${PORT}`)
 })
